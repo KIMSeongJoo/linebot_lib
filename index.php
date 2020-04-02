3c1f92e7cc3edd2862071cb02a1d24d705eb8bd6
KIM seongjoo <kimseongjoo@liful..com>
2020-04-02 16:51:00 +0900

    test

--- index.php
+++ index.php
@@ -38,12 +38,11 @@ try {
 //                $text = file_get_contents('json/start_01.json');
                 $text = preg_replace("/\r|\n/", '', file_get_contents('json/start_01.json'));
 
-                $message = $text;
-//                $message = [
-//                    'type' => \LINE\LINEBot\Constant\MessageType::FLEX,
-//                    'altText' => 'sjkim',
-//                    'contents' => $text
-//                ];
+                $message[]= [
+                    'type' => \LINE\LINEBot\Constant\MessageType::FLEX,
+                    'altText' => 'sjkim',
+                    'contents' => $text
+                ];
                 error_log($text);
 //                $message['type'] = \LINE\LINEBot\Constant\MessageType::TEXT;
 //                $message['text'] = "お友だち登録ありがとう～っ\nLIFULL HOME'S公式アカウントからお得なキャンペーン情報やサービスの案内を配信してるから、楽しみにしててねっ！\n\n通知が気になる場合は、この画面内のトーク設定から「通知」をOFFにしてねっ！これからよろしくねっ！";
