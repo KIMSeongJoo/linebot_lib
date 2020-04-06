9c35790311853c19afee36a370186bf50c42f99c
KIM seongjoo <kimseongjoo@liful..com>
2020-04-06 11:26:29 +0900

    test

--- define.php
+++ define.php
@@ -1,4 +1,7 @@
 <?php
-define('ACCESS_TOKEN','zE1fAeTt5cJPcWQF+tzg95Z+c68u1V1ADxkhIs+8N2qqM5Ntp4M4XEkfpq1+kCX8935PKz/T0fZ83VWH4mMge4brRDYazmoMFj7EWXV/+lWbxMOXE3w9Vvq00C7ZTTu4U9UNh1tqFtjHZesr05zAvAdB04t89/1O/w1cDnyilFU=');  //アクセストークン
-define('CHANNEL_SECRET','df98e010e19d1f9726555aaef6be05a9');  //シークレット
+//define('ACCESS_TOKEN','zE1fAeTt5cJPcWQF+tzg95Z+c68u1V1ADxkhIs+8N2qqM5Ntp4M4XEkfpq1+kCX8935PKz/T0fZ83VWH4mMge4brRDYazmoMFj7EWXV/+lWbxMOXE3w9Vvq00C7ZTTu4U9UNh1tqFtjHZesr05zAvAdB04t89/1O/w1cDnyilFU=');  //アクセストークン
+//define('CHANNEL_SECRET','df98e010e19d1f9726555aaef6be05a9');  //シークレット
+
+define('ACCESS_TOKEN','3ag8XxB+olSWC8+YIxlZzuDtIFXvU1fj2GgA521XaicFR8/pgQGu0J61j3nbvPAVPxKP1lZdBkk9txP1zQ07LQIa4+9Rz8ch1Jk0+DEb8nLA/gq5OqIEpVg5r8aYzyHUnH4iWpNSvmS1kuPf66jaJAdB04t89/1O/w1cDnyilFU=');  //アクセストークン
+define('CHANNEL_SECRET','b56004a061adf2e5cea291d8e9179040');  //シークレット
 ?>
