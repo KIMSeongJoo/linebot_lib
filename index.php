372bcb285e9cabe4f29a55d8ee223da4c584a71f
KIM seongjoo <kimseongjoo@liful..com>
2020-05-14 16:03:04 +0900

    오타수정, 메시지 링크 수정

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 $bot = new LineBotClass();
 $jsonBasePath = "json/proto2/";
 
-$richMenu1 = "richmenu-9960eec5fe93e2a523e3aca95ff8343b";
+$richMenu1 = "richmenu-1fa0e2c37a8690d4e89d7160d5f2524b";
 $exceptionStringList = [
     "3ヶ月以内"
     , "半年以内"
